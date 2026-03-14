97037c0a830d8a9b0d1a3dbe04fcb59aa4449b14
Michelle Postrado <michellepostrado26@gmail.com>
2026-03-15 05:00:48 +0800

    ewan

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: 'https://salisensebackend.onrender.com', 
-
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
     headers: {
         'Content-Type': 'application/json'
     }
